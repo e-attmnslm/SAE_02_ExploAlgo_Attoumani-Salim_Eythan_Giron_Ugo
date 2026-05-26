e83ff4253906ea6f66b3514cca315d74c2d62452
Eythan ATTOUMANI SALIM <eythanattoumanisalim@gmail.com>
2026-05-26 16:59:36 +0200

    ajout de Arcs et Graphe

---
+++ src/Arcs.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Arcs {
+    private List<Arc> liste = new ArrayList<Arc>();
+
+    public void ajouter(Arc a){
+        liste.add(a);
+    }
+    public List<Arc> getListe(){
+        return this.liste;
+    }
+}
